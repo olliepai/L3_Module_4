174d742e8dffed1ed0b07ab2aa12bf46e5790edd
codeavenger1 <league@WTS10.attlocal.net>
2017-09-24 12:57:45 -0700

    Day 2

--- src/threads/OlympicRings_Threaded.java
+++ src/threads/OlympicRings_Threaded.java
@@ -1,7 +1,82 @@
 package threads;
 
+import java.awt.Color;
+
+import javax.swing.JFrame;
+
+import org.jointheleague.graphical.robot.Robot;
+
 public class OlympicRings_Threaded {
-	// Make A Program that uses Threads and robots to draw the Olympic rings. One robot should draw one ring simultaneously with the other 4 robots.
+	// Make A Program that uses Threads and robots to draw the Olympic rings. One
+	// robot should draw one ring simultaneously with the other 4 robots.
+	public static void main(String args[]) {
+		Robot one = new Robot();
+		Robot two = new Robot();
+		Robot three = new Robot();
+		Robot four = new Robot();
+		Robot five = new Robot();
 
+		one.setSpeed(10);
+		two.setSpeed(10);
+		three.setSpeed(10);
+		four.setSpeed(10);
+		five.setSpeed(10);
+
+		one.moveTo(550, 450);
+		two.moveTo(800, 450);
+		three.moveTo(1050, 450);
+		four.moveTo(675, 600);
+		five.moveTo(925, 600);
+
+		one.setPenColor(Color.BLUE);
+		one.setPenWidth(5);
+		one.penDown();
+		two.setPenColor(Color.BLACK);
+		two.setPenWidth(5);
+		two.penDown();
+		three.setPenColor(Color.RED);
+		three.setPenWidth(5);
+		three.penDown();
+		four.setPenColor(Color.YELLOW);
+		four.setPenWidth(5);
+		four.penDown();
+		five.setPenColor(Color.GREEN);
+		five.setPenWidth(5);
+		five.penDown();
+
+		new Thread(() -> {
+			for (int i = 0; i < 60; i++) {
+				one.move(16);
+				one.turn(6);
+			}
+		}).start();
+
+		new Thread(() -> {
+			for (int i = 0; i < 60; i++) {
+				two.move(16);
+				two.turn(6);
+			}
+		}).start();
+
+		new Thread(() -> {
+			for (int i = 0; i < 60; i++) {
+				three.move(16);
+				three.turn(6);
+			}
+		}).start();
+
+		new Thread(() -> {
+			for (int i = 0; i < 60; i++) {
+				four.move(16);
+				four.turn(6);
+			}
+		}).start();
+
+		new Thread(() -> {
+			for (int i = 0; i < 60; i++) {
+				five.move(16);
+				five.turn(6);
+			}
+		}).start();
+	}
 }
-
--- src/threads/ThreadedReverseGreeting.java
+++ src/threads/ThreadedReverseGreeting.java
@@ -1,12 +1,26 @@
+package threads;
 
 public class ThreadedReverseGreeting {
-  //Write a program that creates a thread (Thread 1) that creates another thread (Thread 2); 
-  //Thread 2 creates Thread 3; and so on, up to Thread 50. 
-  //Each thread should print "Hello from Thread <num>!", 
-  //but you should structure your program such that the threads print their greetings in reverse order.
+	// Write a program that creates a thread (Thread 1) that creates another thread
+	// (Thread 2);
+	// Thread 2 creates Thread 3; and so on, up to Thread 50.
+	// Each thread should print "Hello from Thread <num>!",
+	// but you should structure your program such that the threads print their
+	// greetings in reverse order.
 	public static void main(String args[]) {
-		new Thread(()->{
-			new Thread(()->System.out.println("Hello from Thread 2")).start();
+		createThread(50);
+
+	}
+
+	public static void createThread(int name) {
+
+		Thread t = new Thread(() -> {
+			System.out.println("Hello from Thread " + name + "!");
+			if (name > 1) {
+				createThread(name - 1);
+			}
 		});
+		
+		t.start();
 	}
 }
